869a7795b5cf9ee6739e0da909623b9a41d004a6
Andre Aranha <andre@andres-macbook-pro.local>
2015-12-23 20:49:36 -0300

    Initial Commit

---
+++ Fighter Champion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fighter Champion
+//
+//  Created by Andre Aranha on 12/23/15.
+//  Copyright © 2015 Andre Aranha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Fighter Champion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fighter Champion
+//
+//  Created by Andre Aranha on 12/23/15.
+//  Copyright © 2015 Andre Aranha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
